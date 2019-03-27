38df0e040587fef792216e3e99f50ede6fa9856d
damirs11 <damir.calixov1@gmail.com>
2019-03-27 22:06:54 +0500

    Main page 90% --- Need make redirect to Author page

--- js/custom_pagination.js
+++ js/custom_pagination.js
@@ -1,31 +1,65 @@
+var initBlogFeed_status = false;
+
 $(document).ready(function(){
 
-    console.log(db);
-    var res = db.exec("SELECT * FROM myTable")
+    var res = db.exec("SELECT * FROM postsTable")
     console.log(res[0].values.length);
 
-    function simpleTemplating(data) {
-        var html = '<ul class="pagination">';
-        $.each(data, function(index, item){
-                console.log(item);
-                html += '<li class="page-item"><a class="page-link" href="#">'+ (index + 1) + '</a></li>';
+    if(!initBlogFeed_status)
+    {   
+        var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
+        (0 + 1) + 
+        " AND " + 
+        ((0 + 1) + 1))[0].values;
+
+        //countPage(pageNumber - 1) + pageNumber
+        console.log(posts);
+        
+        var html = '<div class="col-sm-8 blog-main">';
+        $.each(posts, function (index, value) { 
+            html += '<div class="blog-post">';
+            html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
+            html += '<p class="blog-post-meta">' + value[2] + ' by <a href="#">'+ value[3] +'</a></p>';
+            html += '<div class="blog-post-content">';
+            html += value[4]
+            html += '</div>'
+            html += '</div>';
         });
-        html += '</ul>';
-        return html;
+        html += '</div>';
+
+        $("#main-content-blog").html(html); // some ajax content loading...
     }
 
-    var html = simpleTemplating(res[0].values);
-    $('#pagination-container').html(html);
-            
-    
     // init bootpag
     $('#page-selection').bootpag({
-        total: res[0].values.length,
+        total: res[0].values.length / 2,
         maxVisible: 5
-    }).on("page", function(event, /* page number here */ num){
-            $("#content").html(num); // some ajax content loading...
-    });
+    }).on("page", function(event, pageNumber){
+
+        var countPage = pageNumber - 1;
+
+        var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
+        (countPage + pageNumber) + 
+        " AND " + 
+        ((countPage + pageNumber) + 1))[0].values;
+
+        //countPage(pageNumber - 1) + pageNumber
+        console.log(posts);
         
+        var html = '<div class="col-sm-8 blog-main">';
+        $.each(posts, function (index, value) { 
+            html += '<div class="blog-post">';
+            html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
+            html += '<p class="blog-post-meta">' + value[2] + ' by <a href="#">'+ value[3] +'</a></p>';
+            html += '<div class="blog-post-content">';
+            html += value[4]
+            html += '</div>'
+            html += '</div>';
+        });
+        html += '</div>';
+
+        $("#main-content-blog").html(html); // some ajax content loading...
+    });
     
 });
 
--- js/sqlite.js
+++ js/sqlite.js
@@ -2,40 +2,42 @@ var db = new SQL.Database();
 
 $(document).ready(function(){
 
-      var dbcreate = 
+      var dbtestcreate = 
             "CREATE TABLE 'myTable' (" +
-            "'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-            "'name' varchar(255) default NULL," +
-            "'company' varchar(255)," +
-            "'email' varchar(255) default NULL" +
+            "'id'       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+            "'name'     varchar(255) default NULL," +
+            "'company'  varchar(255)," +
+            "'email'    varchar(255) default NULL" +
       ");"
-
-      var dataForDB = 
+      var dbtestcreate_Data = 
       "INSERT INTO 'myTable' ('name','company','email') VALUES ('Cade','Pellentesque Associates','sociis.natoque.penatibus@adipiscingelitAliquam.ca'),('Basil','Vitae Risus Duis Consulting','mauris.id@Phasellusfermentum.com'),('Marvin','Pellentesque Habitant Morbi Ltd','elit.pretium.et@miDuis.co.uk'),('Carlos','Faucibus Leo In Inc.','dui@aliquamiaculislacus.com'),('Bert','Placerat Limited','luctus@ornaretortorat.com'),('Devin','Cum Sociis Natoque Inc.','enim.diam.vel@dictummiac.com'),('Rahim','Dictum Augue Limited','Donec@aptenttaciti.org'),('Jermaine','Risus Ltd','adipiscing.elit@tristiquepellentesque.co.uk'),('Julian','Faucibus Orci PC','Sed.et.libero@Sedeget.ca'),('Keaton','Interdum Sed Consulting','Donec.feugiat.metus@porttitor.com');"
 
-      
+      var dbBlogcreate = 
+            "CREATE TABLE 'postsTable' (" +
+            "'id'       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+            "'title'    TEXT default NULL, " +
+            "'date'     TEXT," +
+            "'author'   TEXT default NULL," +
+            "'mainText' TEXT default NULL" +
+      ");"
+      var dbBlogcreate_Data = 
+      "INSERT INTO 'postsTable' ('title','date', 'author', 'mainText')" + 
+      "VALUES ('TitleOfBlog1',      date('now'),                  '~Dew',     'Первая провека рабоыт текста с date(now)')," + 
+      "('TitleOfBlog2',             date('now','-2 months'),      '~Dew',     'Вторая провека рабоыт текста с date(now - 2 месяца)')," + 
+      "('TitleOfBlog3',             date('now','-4 months'),      '~Dew',     'Третья провека рабоыт текста с date(now - 4 месяца)')," + 
+      "('TitleOfBlog4',             date('now','-6 months'),      '~Dew',     'Четвертая провека рабоыт текста с date(now - 6 месяца)');"
 
-      db.run(dbcreate);
-      db.run(dataForDB);
+      db.run(dbtestcreate);
+      db.run(dbtestcreate_Data);
+
+      db.run(dbBlogcreate);
+      db.run(dbBlogcreate_Data);
+
+
 
       var res = db.exec("SELECT * FROM myTable");
-
       console.log(res);
 
-      function ArrayToBinString (arr) {
-            var uarr = new Uint8Array(arr);
-            var strings = [], chunksize = 0xffff;
-            // There is a maximum stack size. We cannot call String.fromCharCode with as many arguments as we want
-            for (var i=0; i*chunksize < uarr.length; i++){
-                  strings.push(String.fromCharCode.apply(null, uarr.subarray(i*chunksize, (i+1)*chunksize)));
-            }
-            return strings.join('');
-      }
-
-      function StrToBinArray (str) {
-            var l = str.length,
-            arr = new Uint8Array(l);
-            for (var i=0; i<l; i++) arr[i] = str.charCodeAt(i);
-            return arr;
-      }
+      var res = db.exec("SELECT * FROM postsTable");
+      console.log(res);
 });
