e25281590efb9602c17b37037a0bcaec888a796c
damirs11 <damir.calixov1@gmail.com>
2019-03-27 16:49:28 +0500

    Main page 50%

--- js/custom_pagination.js
+++ js/custom_pagination.js
@@ -1,24 +1,32 @@
 $(document).ready(function(){
 
-      console.log(db);
-      var res = db.exec("SELECT * FROM myTable")
-      console.log(res[0].values.length);
+    console.log(db);
+    var res = db.exec("SELECT * FROM myTable")
+    console.log(res[0].values.length);
 
-      function simpleTemplating(data) {
-          var html = '<ul class="pagination">';
-          $.each(data, function(index, item){
-                  console.log(item);
-                  html += '<li class="page-item"><a class="page-link" href="#">'+ (index + 1) + '</a></li>';
-          });
-          html += '</ul>';
-          return html;
-      }
+    function simpleTemplating(data) {
+        var html = '<ul class="pagination">';
+        $.each(data, function(index, item){
+                console.log(item);
+                html += '<li class="page-item"><a class="page-link" href="#">'+ (index + 1) + '</a></li>';
+        });
+        html += '</ul>';
+        return html;
+    }
 
-      var html = simpleTemplating(res[0].values);
-      $('#pagination-container').html(html);
-              
-              
-      
+    var html = simpleTemplating(res[0].values);
+    $('#pagination-container').html(html);
+            
+    
+    // init bootpag
+    $('#page-selection').bootpag({
+        total: res[0].values.length,
+        maxVisible: 5
+    }).on("page", function(event, /* page number here */ num){
+            $("#content").html(num); // some ajax content loading...
+    });
+        
+    
 });
 
 
