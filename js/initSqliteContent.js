be8ea6daccf4069a3da51867338c02522dfa9a59
damirs11 <damir.calixov1@gmail.com>
2019-03-28 23:57:37 +0500

    About page 50%

--- js/initSqliteContent.js
+++ js/initSqliteContent.js
@@ -2,17 +2,17 @@ var db = new SQL.Database();
 
 $(document).ready(function(){
 
-      var dbtestcreate = 
+      var dbtest_create = 
             "CREATE TABLE 'myTable' (" +
             "'id'       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
             "'name'     varchar(255) default NULL," +
             "'company'  varchar(255)," +
             "'email'    varchar(255) default NULL" +
       ");"
-      var dbtestcreate_Data = 
+      var dbtest_createData = 
       "INSERT INTO 'myTable' ('name','company','email') VALUES ('Cade','Pellentesque Associates','sociis.natoque.penatibus@adipiscingelitAliquam.ca'),('Basil','Vitae Risus Duis Consulting','mauris.id@Phasellusfermentum.com'),('Marvin','Pellentesque Habitant Morbi Ltd','elit.pretium.et@miDuis.co.uk'),('Carlos','Faucibus Leo In Inc.','dui@aliquamiaculislacus.com'),('Bert','Placerat Limited','luctus@ornaretortorat.com'),('Devin','Cum Sociis Natoque Inc.','enim.diam.vel@dictummiac.com'),('Rahim','Dictum Augue Limited','Donec@aptenttaciti.org'),('Jermaine','Risus Ltd','adipiscing.elit@tristiquepellentesque.co.uk'),('Julian','Faucibus Orci PC','Sed.et.libero@Sedeget.ca'),('Keaton','Interdum Sed Consulting','Donec.feugiat.metus@porttitor.com');"
 
-      var dbBlogcreate = 
+      var dbBlog_create = 
             "CREATE TABLE 'postsTable' (" +
             "'id'       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
             "'title'    TEXT default NULL, " +
@@ -20,18 +20,18 @@ $(document).ready(function(){
             "'author'   TEXT default NULL," +
             "'mainText' TEXT default NULL" +
       ");"
-      var dbBlogcreate_Data = 
+      var dbBlog_createData = 
       "INSERT INTO 'postsTable' ('title','date', 'author', 'mainText')" + 
       "VALUES ('TitleOfBlog1',      date('now'),                  '~Dew',     'Первая провека рабоыт текста с date(now)')," + 
       "('TitleOfBlog2',             date('now','-2 months'),      '~Dew',     'Вторая провека рабоыт текста с date(now - 2 месяца)')," + 
       "('TitleOfBlog3',             date('now','-4 months'),      '~Dew',     'Третья провека рабоыт текста с date(now - 4 месяца)')," + 
       "('TitleOfBlog4',             date('now','-6 months'),      '~Dew',     'Четвертая провека рабоыт текста с date(now - 6 месяца)');"
 
-      db.run(dbtestcreate);
-      db.run(dbtestcreate_Data);
+      db.run(dbtest_create);
+      db.run(dbtest_createData);
 
-      db.run(dbBlogcreate);
-      db.run(dbBlogcreate_Data);
+      db.run(dbBlog_create);
+      db.run(dbBlog_createData);
 
 
 
--- js/mainBlogFeed.js
+++ js/mainBlogFeed.js
@@ -1,66 +1,126 @@
 var initBlogFeed_status = false;
 
-$(document).ready(function(){
+BlogFunction = 
+{   
+    init : function initBlogFeed(){
 
-    var res = db.exec("SELECT * FROM postsTable")
-    console.log(res[0].values.length);
+        $(document).ready(function(){
+            var res = db.exec("SELECT * FROM postsTable")
+            console.log(res[0].values.length);
 
-    if(!initBlogFeed_status) //initFirstPage
-    {   
-        var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
-        (0 + 1) + 
-        " AND " + 
-        ((0 + 1) + 1))[0].values;
+            if(!initBlogFeed_status) //initFirstPage
+            {   
+                initBlogFeed = true;
 
-        //countPage(pageNumber - 1) + pageNumber
-        console.log(posts);
-        
-        var html = '<div class="col-sm-8 blog-main">';
-        $.each(posts, function (index, value) { 
-            html += '<div class="blog-post">';
-            html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
-            html += '<p class="blog-post-meta">' + value[2] + ' by <a href="#">'+ value[3] +'</a></p>';
-            html += '<div class="blog-post-content">';
-            html += value[4]
-            html += '</div>'
-            html += '</div>';
+                $('html, body').animate({ scrollTop: 0 }, 'fast');
+
+                var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
+                (0 + 1) + 
+                " AND " + 
+                ((0 + 1) + 1))[0].values;
+
+                console.log(posts);
+
+                var html = '<div class="row" id="main-content-blog">'
+
+                html += '<div class="blog-header">';
+                html += '<h1 class="blog-title">The Bootstrap Blog</h1>';
+                html += '<p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>';
+                html += '</div>'; //blog-header
+                
+                //<!-- Posts of blog -->
+                
+                html += '<div class="col-sm-8 blog-main">';
+                $.each(posts, function (index, value) { 
+                    html += '<div class="blog-post">';
+                    html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
+                    html += '<p class="blog-post-meta">' + value[2] + ' by <a onclick="AboutMeFunction.init()">'+ value[3] +'</a></p>';
+                    html += '<div class="blog-post-content">';
+                    html += value[4]
+                    html += '</div>'
+                    html += '</div>';
+                });
+                html += '</div>'; //blog-main
+                html += '</div>'; //main-content-blog
+                
+                
+                
+
+                $(".page-content").html(html); // content loading
+            }
+
+            // init bootpag
+            $('#page-selection').bootpag({
+                total: res[0].values.length / 2,
+                maxVisible: 5
+            }).on("page", function(event, pageNumber){
+
+                $('html, body').animate({ scrollTop: 0 }, 'fast');
+
+                var countPage = pageNumber - 1;
+
+                var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
+                (countPage + pageNumber) + 
+                " AND " + 
+                ((countPage + pageNumber) + 1))[0].values
+                console.log(posts);
+
+                var html = '<div class="row" id="main-content-blog">'
+
+                html += '<div class="blog-header">';
+                html += '<h1 class="blog-title">The Bootstrap Blog</h1>';
+                html += '<p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>';
+                html += '</div>'; //blog-header
+                
+                //<!-- Posts of blog -->
+                
+                html += '<div class="col-sm-8 blog-main">';
+                $.each(posts, function (index, value) { 
+                    html += '<div class="blog-post">';
+                    html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
+                    html += '<p class="blog-post-meta">' + value[2] + ' by <a onclick="AboutMeFunction.init()">'+ value[3] +'</a></p>';
+                    html += '<div class="blog-post-content">';
+                    html += value[4]
+                    html += '</div>'
+                    html += '</div>';
+                });
+                html += '</div>'; //blog-main
+                html += '</div>'; //main-content-blog
+
+                
+
+                $(".page-content").html(html); // content loading
+            });
         });
-        html += '</div>';
-
-        $("#main-content-blog").html(html); // content loading
     }
+}
 
-    // init bootpag
-    $('#page-selection').bootpag({
-        total: res[0].values.length / 2,
-        maxVisible: 5
-    }).on("page", function(event, pageNumber){
+AboutMeFunction = {
 
-        var countPage = pageNumber - 1;
+    init : function (){
+        $(document).ready(function(){
 
-        var posts = db.exec("SELECT * FROM postsTable WHERE id BETWEEN " + 
-        (countPage + pageNumber) + 
-        " AND " + 
-        ((countPage + pageNumber) + 1))[0].values;
+            $('html, body').animate({ scrollTop: 0 }, 'fast');
+            $('#page-selection').empty();
 
-        //countPage(pageNumber - 1) + pageNumber
-        console.log(posts);
-        
-        var html = '<div class="col-sm-8 blog-main">';
-        $.each(posts, function (index, value) { 
-            html += '<div class="blog-post">';
-            html += '<h2 class="blog-post-title">'+ value[1] +'</h2>';
-            html += '<p class="blog-post-meta">' + value[2] + ' by <a href="#">'+ value[3] +'</a></p>';
-            html += '<div class="blog-post-content">';
-            html += value[4]
-            html += '</div>'
+            var html = '<div class="blog-header">';
+            html += '<h1 class="blog-title">Обо мне</h1>';
+            html += '<p class="lead blog-description">Краткое описания о себе и роде моей деятельности</p>';
             html += '</div>';
+            
+            html += '<div class="col-sm-8 blog-main">';
+            
+                html += '<div class="blog-post">';
+                html += '<h2 class="blog-post-title"> Заголовок </h2>';
+                html += '<div class="blog-post-content">';
+                html += '<p> Контент </p>'
+                html += '</div>'
+                html += '</div>';
+            
+            html += '</div>';
+
+            $(".page-content").html(html); // content loading
         });
-        html += '</div>';
-
-        $("#main-content-blog").html(html); // content loading
-    });
-    
-});
-
+    } 
+}
 
